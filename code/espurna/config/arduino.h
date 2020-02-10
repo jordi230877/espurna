d4e8b53b51ec27112277894a48993a7272718d7e
Jordi FM <jferret@omatech.com>
2020-02-10 11:39:39 +0100

    Add status aggregation topic and disable button direct action on relay

--- code/espurna/config/arduino.h
+++ code/espurna/config/arduino.h
@@ -3,6 +3,9 @@
 // Uncomment the appropiate line(s) to build from the Arduino IDE
 //--------------------------------------------------------------------------------
 
+#define INTERNAL_RELAY_CONTROL 0
+#define AGG_HEARTBEAT 1
+
 #pragma once
 
 //--------------------------------------------------------------------------------
